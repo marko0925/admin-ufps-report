650383b18f270792b9e168129c9a35319c113e26
Cristian Ramirez <crisdaraca@hotmail.com>
2017-11-26 13:51:41 -0500

    Estructura html finalizada

---
+++ dist/js/procesar-crud1.js
@@ -0,0 +1,36 @@
+
+function registrarSala() {
+    $(".seccioninfo").hide();
+    $("#registrar-salas").show();
+
+}
+
+function consultarSala() {
+    $(".seccioninfo").hide();
+    $("#consultar-salas").show();
+
+}
+
+function registrarHorario() {
+    $(".seccioninfo").hide();
+    $("#registrar-horario").show();
+
+}
+
+function consultarHorario() {
+    $(".seccioninfo").hide();
+    $("#consultar-horario").show();
+
+}
+
+function registrarBeca() {
+    $(".seccioninfo").hide();
+    $("#registrar-beca").show();
+
+}
+
+function consultarBeca() {
+    $(".seccioninfo").hide();
+    $("#consultar-beca").show();
+
+}
